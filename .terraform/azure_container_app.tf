f76904043478a76f0a12339862470b32231a3f4f
Sean Connelly <47223469+2good4hisowngood@users.noreply.github.com>
2024-07-01 19:20:30 +0000

    chore: Update tfplan artifact path in GitHub Actions workflow

--- .terraform/azure_container_app.tf
+++ .terraform/azure_container_app.tf
@@ -26,7 +26,7 @@ resource "azurerm_container_app" "example" {
   template {
     container {
       name   = "examplecontainerapp"
-      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      image  = var.image_name # "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
       cpu    = 0.25
       memory = "0.5Gi"
     }
