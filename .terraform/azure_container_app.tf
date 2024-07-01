80f759b1386e8e3a697eb085642ab2fc56aad80c
Sean Connelly <47223469+2good4hisowngood@users.noreply.github.com>
2024-07-01 19:53:19 +0000

    chore: Update Azure Container App to use UserAssigned identity for authentication

--- .terraform/azure_container_app.tf
+++ .terraform/azure_container_app.tf
@@ -17,15 +17,21 @@ resource "azurerm_container_app_environment" "example" {
   resource_group_name        = azurerm_resource_group.example.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 }
+
 resource "azurerm_container_app" "example" {
   name                         = "example-app"
   container_app_environment_id = azurerm_container_app_environment.example.id
   resource_group_name          = azurerm_resource_group.example.name
   revision_mode                = "Single"
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.example.id,
+    ]
+  }
   registry {
     server   = var.acr_server
-    username = var.acr_username
-    password = var.acr_password
+    identity = azurerm_container_app_environment.example.identity
   }
 
   template {
@@ -34,6 +40,21 @@ resource "azurerm_container_app" "example" {
       image  = var.image_name
       cpu    = 0.25
       memory = "0.5Gi"
+      env {
+        name  = "REPO"
+        value = "2good4hisowngood/Terraform-tech-talk"
+      }
+      env {
+        name = TOKEN
+        value = var.org_name
+      }
     }
   }
 }
+
+resource "azurerm_user_assigned_identity" "example" {
+  location            = azurerm_resource_group.example.location
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
