ab7734e7b98864ca148fcc540f176134758caf2d
Sean Connelly <47223469+2good4hisowngood@users.noreply.github.com>
2024-07-01 15:49:14 +0000

    chore: Update .terraform/outputs.tf to use repo_ids instead of repo_id

--- .terraform/outputs.tf
+++ .terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "repositories" {
-    value = tolist(data.github_repositories.example.repositories[*].id) # [github_repository.example.repo_id]
+    value = data.github_repositories.example.repo_ids
 }
