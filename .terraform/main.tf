8d617adc627fb3609ea7b55fc1b493a6cef36c5a
Sean Connelly <47223469+2good4hisowngood@users.noreply.github.com>
2024-07-02 15:33:53 -0500

    Update main.tf
    removing unnecessary bits

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -1,5 +1,3 @@
-# https://registry.terraform.io/providers/integrations/github/latest/docs/resources/actions_runner_group
-
 data "github_repositories" "example" {
   query = "org:${var.org_name}"
   include_repo_id = true
